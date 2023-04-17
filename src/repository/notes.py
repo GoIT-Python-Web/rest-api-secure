5637e6aa5b20b9331f6a3e52344d3dc88c4e563a
Yuriy Kuchma <krabatua@gmail.com>
2023-04-17 12:51:25 +0300

    fix

--- src/repository/notes.py
+++ src/repository/notes.py
@@ -4,19 +4,25 @@ from sqlalchemy import and_
 from sqlalchemy.orm import Session
 
 from src.database.models import Note, Tag, User
-from src.shemas import NoteModel, NoteUpdate, NoteStatusUpdate
+from src.schemas import NoteModel, NoteUpdate, NoteStatusUpdate
 
 
 async def get_notes(skip: int, limit: int, user: User, db: Session) -> List[Note]:
-    return db.query(Note).filter(Note.user_id == user.id).offset(skip).limit(limit).all()
+    return (
+        db.query(Note).filter(Note.user_id == user.id).offset(skip).limit(limit).all()
+    )
 
 
 async def get_note(note_id: int, user: User, db: Session) -> Note:
-    return db.query(Note).filter(and_(Note.id == note_id, Note.user_id == user.id)).first()
+    return (
+        db.query(Note).filter(and_(Note.id == note_id, Note.user_id == user.id)).first()
+    )
 
 
 async def create_note(body: NoteModel, user: User, db: Session) -> Note:
-    tags = db.query(Tag).filter(and_(Tag.id.in_(body.tags), Tag.user_id == user.id)).all()
+    tags = (
+        db.query(Tag).filter(and_(Tag.id.in_(body.tags), Tag.user_id == user.id)).all()
+    )
     note = Note(title=body.title, description=body.description, tags=tags, user=user)
     db.add(note)
     db.commit()
@@ -25,17 +31,27 @@ async def create_note(body: NoteModel, user: User, db: Session) -> Note:
 
 
 async def remove_note(note_id: int, user: User, db: Session) -> Note | None:
-    note = db.query(Note).filter(and_(Note.id == note_id, Note.user_id == user.id)).first()
+    note = (
+        db.query(Note).filter(and_(Note.id == note_id, Note.user_id == user.id)).first()
+    )
     if note:
         db.delete(note)
         db.commit()
     return note
 
 
-async def update_note(note_id: int, body: NoteUpdate, user: User, db: Session) -> Note | None:
-    note = db.query(Note).filter(and_(Note.id == note_id, Note.user_id == user.id)).first()
+async def update_note(
+    note_id: int, body: NoteUpdate, user: User, db: Session
+) -> Note | None:
+    note = (
+        db.query(Note).filter(and_(Note.id == note_id, Note.user_id == user.id)).first()
+    )
     if note:
-        tags = db.query(Tag).filter(and_(Tag.id.in_(body.tags), Note.user_id == user.id)).all()
+        tags = (
+            db.query(Tag)
+            .filter(and_(Tag.id.in_(body.tags), Note.user_id == user.id))
+            .all()
+        )
         note.title = body.title
         note.description = body.description
         note.done = body.done
@@ -44,8 +60,12 @@ async def update_note(note_id: int, body: NoteUpdate, user: User, db: Session) -
     return note
 
 
-async def update_status_note(note_id: int, body: NoteStatusUpdate, user: User, db: Session) -> Note | None:
-    note = db.query(Note).filter(and_(Note.id == note_id, Note.user_id == user.id)).first()
+async def update_status_note(
+    note_id: int, body: NoteStatusUpdate, user: User, db: Session
+) -> Note | None:
+    note = (
+        db.query(Note).filter(and_(Note.id == note_id, Note.user_id == user.id)).first()
+    )
     if note:
         note.done = body.done
         db.commit()
